ab1f481ca247b97a9c08882504269c6dc7e11c3c
visualstudio <visualstudio@122-PC28>
2019-03-09 17:37:34 -0500

    village

---
+++ ConsoleApp4/List.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    public class List
+    {
+        internal void Add(JournalEntry journalEntry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
